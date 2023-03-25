2026cfcbf861aa67975fd7b991f0b937e262da5e
Ilya Malyshev <55033489+malyshevig@users.noreply.github.com>
2023-03-25 17:20:02 +0300

    Code Improvement

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient, events
 import asyncio
 import logging
-import dao
+from  dao import BooksDAO
 import sys
 
 import local_types
@@ -10,61 +10,76 @@ from telethon.tl.custom.file import File
 from telethon.tl.custom.message import Message
 from telethon.tl.custom.dialog import Dialog
 
-logging.basicConfig(encoding='utf-8', format='%(asctime)s %(levelname)s %(message)s', level=logging.ERROR)
+logging.basicConfig(stream=sys.stdout,encoding='utf-8', format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
 api_id = 29093757
 api_hash = "0d64c0d51b514dd14b4efad572cbb5db"
 
-dao = dao.BooksDAO()
 
+class TgScanner:
+    def __init__(self, client:TelegramClient, booksDao: BooksDAO):
+        self.client = client
+        self.dao = booksDao
 
-async def main(client: TelegramClient):
-    try:
-        for d in await client.get_dialogs():
-            dialog = local_types.get_dialog(d)
+    def tg_scan(self):
+        client = self.client
+        with client:
+            client.loop.run_until_complete(self.__scan__(client, self.dao))
 
-            if dialog.dlg_type == "channel":
-                dialog = dao.add_dialog(dialog)
-                print(f"Walk through channel {dialog.name}")
+    async def __scan__(self, client: TelegramClient, dao:BooksDAO):
+        books = 0
+        try:
+            for d in await client.get_dialogs():
+                dialog = local_types.get_dialog(d)
 
-                max_msg_dt = dialog.max_msg_dt
-                msg_iter = client.iter_messages(d, reverse=True) if max_msg_dt is None else \
-                    client.iter_messages(d, offset_date=max_msg_dt, reverse=True)
+                if dialog.dlg_type == "channel":
+                    dialog = dao.add_dialog(dialog)
+                    logging.info(f"Scanning channel {dialog.name}")
 
-                async for m in msg_iter:
-                    msg: Message = m
-                    message: LMessage = local_types.get_message(m, dialog.tg_id)
+                    max_msg_dt = dialog.max_msg_dt
+                    msg_iter = client.iter_messages(d, reverse=True) if max_msg_dt is None else \
+                        client.iter_messages(d, offset_date=max_msg_dt, reverse=True)
 
-                    if msg.file is not None and msg.file.name is not None and msg.file.ext in [".pdf", ".epub", ".fb2"]:
-                        file: LBook = local_types.get_file(msg.file, dialog.tg_id, message.tg_id)
-                        lbook = dao.get_book_by_name(file.filename)
+                    async for m in msg_iter:
+                        msg: Message = m
+                        message: LMessage = local_types.get_message(m, dialog.tg_id)
 
-                        if lbook is None:
-                            dao.add_message(message)
-                            dao.add_book(file)
-                            await msg.download_media("/Volumes/SSD/download")
-                            print(f"add file {file.filename}")
-                        else:
-                            # dao.add_message(message)
-                            if lbook.msg_id is None:
+                        if msg.file is not None and msg.file.name is not None and msg.file.ext in [".pdf", ".epub", ".fb2"]:
+                            file: LBook = local_types.get_file(msg.file, dialog.tg_id, message.tg_id)
+                            lbook = dao.get_book_by_name(file.filename)
+
+                            if lbook is None:
                                 dao.add_message(message)
+                                dao.add_book(file)
+                                books += 1
+                                await msg.download_media("/Volumes/SSD/download")
+                                logging.info(f"add file {file.filename}")
+                            else:
+                                # dao.add_message(message)
+                                if lbook.msg_id is None:
+                                    dao.add_message(message)
 
-                                dao.update_by_name(file.filename, file)
-                                print(f"update file {file.filename}")
+                                    dao.update_by_name(file.filename, file)
+                                    logging.info(f"update file {file.filename}")
 
-                    dialog.max_msg_dt = message.dt
-                    #dao.update_dialog(dialog)
-                dao.update_dialog(dialog)
+                        dialog.max_msg_dt = message.dt
 
-    except Exception as e:
-        logging.error(e)
-        print(e)
-        raise e
+                    dao.update_dialog(dialog)
+            client.disconnect()
 
-    print ("Finished")
+        except Exception as e:
+            logging.error(e)
+            print(e)
+            raise e
+
+        logging.info("Scan finished")
 
 
+def main():
+    dao = BooksDAO()
+    client = TelegramClient("im", api_id, api_hash)
 
-client = TelegramClient("im", api_id, api_hash)
-with client:
-    client.loop.run_until_complete(main(client))
+    TgScanner(client,dao).tg_scan()
+
+if __name__ == "__main__":
+    main()
